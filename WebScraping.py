bc37756e8f7418221c2f0f03f251e9fd5cc15519
shayase <ShunsukeHayase@shayase.local>
2015-03-04 02:49:24 +0900

    get image

--- WebScraping.py
+++ WebScraping.py
@@ -2,10 +2,39 @@
 # -*- coding:utf-8 -*-
 
 import urllib2
-url = ''
-tag = ''
-response = urllib2.urlopen(url)
-html = response.readlines()
-for i in html:
-    if tag in i:
-        print i
+import sys
+import os
+from HTMLParser import HTMLParser, HTMLParseError
+
+class TestHTMLParser(HTMLParser):
+
+    def __init__(self,tag):
+        HTMLParser.__init__(self)
+        self.tag = tag
+        os.mkdir(self.tag)
+    
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if tag == self.tag:
+            self.getImage(attrs["src"])
+
+    def getImage(self, url):
+        filename = self.tag + '/' + self.setName(url)
+        r = urllib2.urlopen(url)
+        f = open(filename, "wb")
+        f.write(r.read())
+        f.close()
+        r.close()
+
+    def setName(self, url):
+        urlsp   = url.split('/')
+        name    = urlsp[-1]
+        return name
+
+url = 'http://www.dmm.co.jp/mono/dvd/-/detail/=/cid=ipz549/'
+
+parser = TestHTMLParser('img')
+r = urllib2.urlopen(url)
+htmldata = r.read()
+parser.feed(htmldata)
+parser.close()
